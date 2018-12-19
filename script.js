6ab715271d05edc66d70c88ba52750aaa991c6d2
novaQuach <nova.quach@gmail.com>
2018-12-18 22:21:21 -0500

    fixed contact form styles

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 AOS.init();
+$(document).ready(function() {
+    $('#home').click(function() {
+        $('.landing-header').scrollIntoView();
+    });
+
+    $('#about').click(function() {
+        $('#about-me-section').scrollIntoView();
+    });
+});
