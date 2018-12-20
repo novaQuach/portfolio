20de7ad83babb172f1b4c1dd6230d8f8ddbc2719
novaQuach <nova.quach@gmail.com>
2018-12-20 15:10:09 -0500

    bio edit

--- script.js
+++ script.js
@@ -1,15 +1,7 @@
 AOS.init();
 
 $(document).ready(function() {
-    // $('#home').click(function(e) {
-    //     e.preventDefault();
-    //     $('.landing-header').get(0).scrollIntoView(true);
-    // });
-
-    // $('#about').click(function(e) {
-    //     e.preventDefault();
-    //     $('#about-me-section').get(0).scrollIntoView(true);
-    // });
-
-
+    $(function() {
+        $('a').smoothScroll();
+    });
 });
