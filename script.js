0decbb707d9eaf7d15384fc833985a10d4359115
novaQuach <nova.quach@gmail.com>
2018-12-29 21:55:51 -0500

    Typed text effect, fix styles folder name

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 AOS.init();
 
+let typed = new Typed('#typed', {
+    strings: [
+        '&nbsp;Developer',
+        '&nbsp;Yoga Lover',
+        '&nbsp;Traveller',
+        '&nbsp;Boba Sipper',
+    ],
+    typeSpeed: 70,
+    backSpeed: 35,
+    backDelay: 1000,
+    loop: true,
+    smartBackspace: true,
+    showCursor: false,
+});
+
 $(document).ready(function() {
     $(function() {
         $('a').smoothScroll();
